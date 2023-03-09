9be2630045a6eddbab2d491bd44338172701ac65
JuanesArango5647896 <110996220+JuanesArango5647896@users.noreply.github.com>
2023-03-08 19:14:51 -0500

    Final de la actividad

--- MiAplicacion/app/src/main/java/com/juanes/miaplicacion/MainActivity2.java
+++ MiAplicacion/app/src/main/java/com/juanes/miaplicacion/MainActivity2.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,7 +17,7 @@ public class MainActivity2 extends AppCompatActivity {
     TextView dato1;
     TextView dato2;
     TextView dato3;
-    Button AbrirGoogle, Llamada;
+    Button AbrirGoogle, Llamada, wifi, Camara, Contacto;
     String phoneNumber = "3022909793";
 
     @Override
@@ -38,6 +41,21 @@ public class MainActivity2 extends AppCompatActivity {
             public void onClick(View view) {
                 llamada(view);
             }
+        });
+        wifi = findViewById(R.id.Wifi);
+        wifi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { openWifiSettings(); }
+        });
+        Camara = findViewById(R.id.Camara);
+        Camara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { gotoCamara(); }
+        });
+        Contacto = findViewById(R.id.Contacto);
+        Contacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { gotoContactos(); }
         });
 
         //Obtengo el de lo recibido el dato(un String que se envio)
@@ -65,5 +83,25 @@ public class MainActivity2 extends AppCompatActivity {
         if(llamar.resolveActivity(getPackageManager())!=null){
             startActivity(llamar);
         }
+    }
+
+    public void openWifiSettings(){
+        Intent verConfiguracion = new Intent(Settings.ACTION_WIFI_SETTINGS);
+        if(verConfiguracion.resolveActivity(getPackageManager())!=null){
+            startActivity(verConfiguracion);
+        }
+    }
+
+    public void gotoCamara(){
+        Intent verCamara = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(verCamara.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(verCamara, 1);
+        }
+    }
+
+    public void gotoContactos(){
+        Intent verContactos = new Intent(Intent.ACTION_PICK);
+        verContactos.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        startActivityForResult(verContactos, 1);
     }
 }
