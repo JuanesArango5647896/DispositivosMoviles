7c0fb160759fddc1f3b06f5d991d66bd4a334be6
JuanesArango5647896 <110996220+JuanesArango5647896@users.noreply.github.com>
2023-04-14 18:07:11 -0500

    Primer Commit

---
+++ MiAplicacion2/app/src/main/java/com/juanes/miaplicacion/MainActivity.java
@@ -0,0 +1,36 @@
+package com.juanes.miaplicacion;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<ListaElementos> elementos;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public void  init(){
+        elementos = new ArrayList<>();
+        elementos.add(new ListaElementos("#7B1FA2","Pedro","Ciudad de México","Activo"));
+
+
+        ListAdapter listAdapter = new ListAdapter(elementos,this);
+        RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(listAdapter);
+    }
+}
