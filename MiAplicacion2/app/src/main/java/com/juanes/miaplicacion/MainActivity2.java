c234f86b212fe4abd430a81f16cb4455077edac5
JuanesArango5647896 <110996220+JuanesArango5647896@users.noreply.github.com>
2023-04-17 18:40:36 -0500

    Este si es el Commit final

--- MiAplicacion2/app/src/main/java/com/juanes/miaplicacion/MainActivity2.java
+++ MiAplicacion2/app/src/main/java/com/juanes/miaplicacion/MainActivity2.java
@@ -1,12 +1,16 @@
 package com.juanes.miaplicacion;
 
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    ImageView icono;
 
     TextView name2, ciudad2, estado2, hora2;
 
@@ -16,10 +20,13 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         ListaElementos element = (ListaElementos) getIntent().getSerializableExtra("ListaElementos");
+        icono = findViewById(R.id.icono);
         name2 = findViewById(R.id.name);
         ciudad2 = findViewById(R.id.ciudad);
         estado2 = findViewById(R.id.estado);
         hora2 = findViewById(R.id.hora);
+
+        icono.setColorFilter(Color.parseColor(element.getColor()), PorterDuff.Mode.SRC_IN);
 
         name2.setText(element.getName());
         name2.setTextColor(Color.parseColor(element.getColor()));
